33c95018a0f3979cea1345d4045c892ab286e689
FRANCO1357 <fmelani13@gmail.com>
2022-07-14 14:56:21 +0200

    html & css

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log('Vs ok', Vue);
+
+Vue.config.devtools = true;
+
+const data = {
+    user: {
+      name: 'Nome Utente',
+      avatar: '_io'
+    },
+    contacts: [
+      {
+        name: 'Michele',
+        avatar: '_1',
+      },
+      {
+        name: 'Fabio',
+        avatar: '_2',
+      },
+      {
+        name: 'Samuele',
+        avatar: '_3',
+      },
+      {
+        name: 'Luisa',
+        avatar: '_4',
+      },
+    ]
+  }
