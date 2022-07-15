0a525cb2a76a711098082cafb12ac2a33c1d9a23
FRANCO1357 <fmelani13@gmail.com>
2022-07-15 09:32:33 +0200

    vue contacts

--- script.js
+++ script.js
@@ -2,27 +2,33 @@ console.log('Vs ok', Vue);
 
 Vue.config.devtools = true;
 
-const data = {
-    user: {
-      name: 'Nome Utente',
-      avatar: '_io'
+const root = new Vue({
+    name: 'whatsapp',
+    el: '#container',
+    data: {
+        user: {
+            name: 'Nome Utente',
+            avatar: '_io'
+        },
+        contacts: [
+        {
+            name: 'Michele',
+            avatar: '_1',
+        },
+        {
+            name: 'Fabio',
+            avatar: '_2',
+        },
+        {
+            name: 'Samuele',
+            avatar: '_3',
+        },
+        {
+            name: 'Luisa',
+            avatar: '_4',
+        },
+        ]
     },
-    contacts: [
-      {
-        name: 'Michele',
-        avatar: '_1',
-      },
-      {
-        name: 'Fabio',
-        avatar: '_2',
-      },
-      {
-        name: 'Samuele',
-        avatar: '_3',
-      },
-      {
-        name: 'Luisa',
-        avatar: '_4',
-      },
-    ]
-  }
+    
+});
+
